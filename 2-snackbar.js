b867fa427a31dd64921b66aa474779e945c6e2ec
MalskoSo1 <filani9818@gmail.com>
2025-11-14 20:05:39 +0000

    Deploying to gh-pages from @ MalskoSo1/goit-js-hw-10@311e92c9e3610f2544551a7e433d31833cde83c0 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -4,12 +4,12 @@ import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                
             <input type="number" name="delay" required min="0" />
             <span class="arrow-up arrow">
               <svg width="16" height="16">
-                <use href="./img/sprite.svg#icon-arrow-up"></use>
+                <use href="./sprite.svg#icon-arrow-up"></use>
               </svg>
             </span>
             <span class="arrow-down arrow">
               <svg width="16" height="16">
-                <use href="./img/sprite.svg#icon-arrow-down"></use>
+                <use href="./sprite.svg#icon-arrow-down"></use>
               </svg>
             </span>
           </div>
@@ -27,7 +27,7 @@ import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                
             />
             <span class="custom">
               <svg class="icon-custom-rectangle" width="12" height="12">
-                <use href="./img/sprite.svg#icon-rectangle"></use>
+                <use href="./sprite.svg#icon-rectangle"></use>
               </svg>
             </span>
             Fulfilled
@@ -42,12 +42,12 @@ import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                
             />
             <span class="custom">
               <svg class="icon-custom-rectangle" width="12" height="12">
-                <use href="./img/sprite.svg#icon-rectangle"></use>
+                <use href="./sprite.svg#icon-rectangle"></use>
               </svg>
             </span>
             Rejected
           </label>
         </fieldset>
 
-        <button type="submit">Create notification</button>`,o={form:document.querySelector(".form")};o.form.innerHTML=l;const i=document.querySelector(".input-wrapper"),c=t=>{const e=t.target.closest(".arrow");if(!e)return;const s=i.querySelector("input");e.classList.contains("arrow-up")&&s.stepUp(),e.classList.contains("arrow-down")&&s.value>0&&s.stepDown()},u=t=>{t.preventDefault();const e=document.querySelector('input[name="delay"]').value,s=document.querySelector('.form input[name="state"]:checked');new Promise((a,n)=>{setTimeout(()=>{s.value==="fulfilled"?a():n()},e)}).then(()=>r.show({backgroundColor:"#59a10d",progressBarColor:"#326101",messageColor:"#fff",position:"topRight",message:`✅ Fulfilled promise in ${e}ms`})).catch(()=>r.show({backgroundColor:"#ef4040",progressBarColor:"#b51b1b",messageColor:"#fff",position:"topRight",message:`❌ Rejected promise in ${e}ms`}))};o.form.addEventListener("submit",u);i.addEventListener("click",c);
+        <button type="submit">Create notification</button>`,o={form:document.querySelector(".form")};o.form.innerHTML=l;const a=document.querySelector(".input-wrapper"),c=t=>{const e=t.target.closest(".arrow");if(!e)return;const s=a.querySelector("input");e.classList.contains("arrow-up")&&s.stepUp(),e.classList.contains("arrow-down")&&s.value>0&&s.stepDown()},u=t=>{t.preventDefault();const e=document.querySelector('input[name="delay"]').value,s=document.querySelector('.form input[name="state"]:checked');new Promise((n,i)=>{setTimeout(()=>{s.value==="fulfilled"?n():i()},e)}).then(()=>r.show({backgroundColor:"#59a10d",progressBarColor:"#326101",messageColor:"#fff",position:"topRight",message:`✅ Fulfilled promise in ${e}ms`})).catch(()=>r.show({backgroundColor:"#ef4040",progressBarColor:"#b51b1b",messageColor:"#fff",position:"topRight",message:`❌ Rejected promise in ${e}ms`}))};o.form.addEventListener("submit",u);a.addEventListener("click",c);
 //# sourceMappingURL=2-snackbar.js.map
